a7644355b4b6ecffec551eb27750f317a4b0e845
Donald Plummer <donald.plummer@gmail.com>
2014-07-05 09:08:23 -0700

    Add default expires_in for http caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :default_expires_in
+
+  def default_expires_in
+    expires_in 24.hours, :public => true
+  end
 end
