94e0bfc2a06db408b18abe9f79b8d6f77f1f9dfc
Donald Plummer <donald.plummer@gmail.com>
2014-07-05 14:10:56 -0700

    Add simple color search

--- lib/card_search.rb
+++ lib/card_search.rb
@@ -5,16 +5,44 @@ class CardSearch
     @query = query
   end
 
+  COLOR_LOOKUP = {
+    'w' => 'White',
+    'u' => 'Blue',
+    'b' => 'Black',
+    'r' => 'Red',
+    'g' => 'Green',
+  }.freeze
+
   def cards
     scope = Card
 
     query.split(' ').each do |part|
-      if part.index(':')
-        key, value = part.split(':')
-        case key
-        when 'e'
-          scope = scope.joins(:editions => :mtg_set).
-            where(mtg_sets: {code: value.upcase})
+      case part
+      when /e:([a-z0-9])/i # Edition code
+        value = $1 
+        scope = scope.joins(:editions => :mtg_set).
+          where(mtg_sets: {code: value.upcase})
+
+      when /c(!|:)([wubrgml]+)/i # color
+        comparitor = $1
+        values = $2
+        queries = []
+
+        values.each_char do |letter|
+          case letter
+          when /[wubrg]/
+            queries << "'#{COLOR_LOOKUP.fetch(letter)}' = ANY(colors)"
+          when 'm'
+            scope = scope.where("array_length(colors, 1) > 1")
+          when 'l'
+            queries << "'Land' = ANY(card_types)"
+          end
+        end
+
+        if comparitor == ':'
+          scope = scope.where(queries.join(" OR "))
+        elsif comparitor == '!'
+          scope = scope.where(queries.join(" AND "))
         end
       else
         if part =~ /^AE/
@@ -23,6 +51,8 @@ class CardSearch
         scope = scope.where("unaccent(cards.name) ILIKE ?", "%#{part}%")
       end
     end
+
+    Rails.logger.warn scope.to_sql
 
     cards = scope.
       order("name ASC").
